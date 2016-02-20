c5d2ad37cafddb79f3c53b8d2ad75e05231fe48e
Ronald Liu <sendronald@gmail.com>
2016-02-20 13:15:05 -0800

    Add Camera2 and CameraRun2 Changed TiltShooter command

--- ESMrobot2016/src/org/usfirst/frc/team4669/robot/commands/CameraRun2.java
+++ ESMrobot2016/src/org/usfirst/frc/team4669/robot/commands/CameraRun2.java
@@ -1,47 +1,42 @@
 package org.usfirst.frc.team4669.robot.commands;
 
 import org.usfirst.frc.team4669.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
-public class TiltShooter extends Command {
-	private double encoderValueToTurn = 0;
-	private double EncoderValueForOneRevolution = 0;
-	
-    public TiltShooter(double degree) {
+public class CameraRun2 extends Command {
+
+    public CameraRun2() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	encoderValueToTurn = degree/360 *(EncoderValueForOneRevolution);
-    	requires(Robot.shooter);
+    	requires(Robot.camera2);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.camera2.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	if (Robot.shooter.getTiltEncoder() > (encoderValueToTurn)) {
-    		Robot.shooter.setTiltMotorSpeed(-1.0);
-    	}
+    	Robot.camera2.run();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.shooter.getTiltEncoder() <= (encoderValueToTurn);
+        return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.shooter.setTiltMotorSpeed(0);
+    	Robot.camera2.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	end();
     }
 }
