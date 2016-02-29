a16d9999ec1b87b4ac2fe4d81907028c39a9481c
tand <tand@tand-mba-pc>
2016-02-29 09:04:08 -0800

    add vision code methods

--- ESMrobot2016/src/org/usfirst/frc/team4669/robot/subsystems/Vision.java
+++ ESMrobot2016/src/org/usfirst/frc/team4669/robot/subsystems/Vision.java
@@ -1,5 +1,15 @@
-
 package org.usfirst.frc.team4669.robot.subsystems;
+
+import java.util.ArrayList;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
 
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -12,10 +22,22 @@ public class Vision extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	private Relay lightRelay;
+	private VideoCapture videoCapture;
 	
 	public Vision() {
 		super();
 		lightRelay = new Relay(0);
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		videoCapture = new VideoCapture();  
+		videoCapture.open(0);
+		if(!videoCapture.isOpened())
+		{
+			System.out.println("Video not open");
+		}
+	}
+	
+	public void releaseCapture() {
+		videoCapture.release();
 	}
 	
 	public void turnOnLight() {
@@ -26,32 +48,59 @@ public class Vision extends Subsystem {
 		lightRelay.set(Relay.Value.kOff);
 	}
 	
-	public void getImage() {
-		
+	public Mat getCameraImage() {
+		Mat frame = new Mat();
+		videoCapture.retrieve(frame);
+		return frame;
 	}
 	
-	public void getDifference() {
-		
+	public Mat getDifference(Mat frame1, Mat frame2) {
+		Mat diff = new Mat();
+		Core.subtract(frame2, frame1, diff);
+		return diff;
 	}
 	
-	public void getGrayscale() {
-		
+	public Mat getGrayscale(Mat frame) {
+		Mat grayscale = new Mat();
+		Imgproc.cvtColor(frame, grayscale, Imgproc.COLOR_RGB2GRAY);
+		return grayscale;
 	}
 	
-	public void threshold() {
-		
+	public Mat threshold(Mat frame) {
+		Mat newFrame = new Mat();
+		Imgproc.threshold(frame, newFrame, 50, 255, Imgproc.THRESH_BINARY);
+		return newFrame;
 	}
 	
-	public void getContours() {
-		
+	public ArrayList<MatOfPoint> getContours(Mat frame) {
+		ArrayList<MatOfPoint> contoursList = new ArrayList<MatOfPoint>();
+		Mat hierarchy = new Mat();
+		Imgproc.findContours(frame, contoursList, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+//		Scalar color = new Scalar( 255, 0, 0 );
+//		Imgproc.drawContours(frame, contoursList, -1, color, 3);
+		return contoursList;
 	}
 	
-	public void getMaxContour() {
-		
+	public MatOfPoint getMaxContour(ArrayList<MatOfPoint> contoursList) {
+		MatOfPoint maxContour = new MatOfPoint();
+		if (contoursList.size()>0) {
+			maxContour = contoursList.get(0);
+			double maxArea = Imgproc.contourArea(maxContour);
+			for (MatOfPoint contour:contoursList) {
+				double area = Imgproc.contourArea(contour);
+				if (area>maxArea) {
+					maxContour = contour;
+					maxArea = area;
+				}
+			}
+		}
+		return maxContour;
 	}
 	
-	public void getConvexHull() {
-		
+	public MatOfInt getConvexHull(MatOfPoint contour) {
+		MatOfInt hull = new MatOfInt();
+		Imgproc.convexHull(contour, hull);
+		return hull;
 	}
 
     public void initDefaultCommand() {
@@ -59,4 +108,3 @@ public class Vision extends Subsystem {
         //setDefaultCommand(new MySpecialCommand());
     }
 }
-
